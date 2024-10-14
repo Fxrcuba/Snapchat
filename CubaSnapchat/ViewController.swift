1d0ff311f791e20fce95de789062e6672c7b4ef7
Maria Fernanda Cuba Moriano <maria.cuba@tecsup.edu.pe>
2024-10-14 17:05:42 -0500

    Initial Commit

---
+++ CubaSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CubaSnapchat
+//
+//  Created by Fernanda Cuba on 14/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
